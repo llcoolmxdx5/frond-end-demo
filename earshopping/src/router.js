feae76b954890022b6e80977ccea7b78ac2d89bc
llcoolmxdx5 <2938129812@qq.com>
2019-05-25 18:58:47 +0800

    shop-start-router

--- earshopping/src/router.js
+++ earshopping/src/router.js
@@ -1,23 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
+  mode: 'history',
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  window.document.title = to.meta.title
+  next()
+})
+
+router.afterEach((to, from, next) => {
+  window.screenTop(0, 0)
+})
+
+export default router
