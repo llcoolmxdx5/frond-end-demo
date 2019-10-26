3d61ea39c6db0c1904e9f46818b7d86ccf23f7ad
llcoolmxdx5 <2938129812@qq.com>
2019-10-26 21:36:35 +0800

    Update main.js

--- AAPLEar/server/main.js
+++ AAPLEar/server/main.js
@@ -3,8 +3,8 @@ const app = express();
 
 module.exports = (function () {
     let server = app.listen(8081, function () {
-        let host = '10.9.42.247'
-        let port = server.address().port
+        let host = '10.9.42.247';
+        let port = server.address().port;
         console.log(`应用实例，访问地址为 http://${host}:${port}`)
     })
-})()
+})();
